756d8c7e02325426c5cd2fb1dde5024f6ce67b48
Poojitha Irosha <poojithairosha9311@gmail.com>
2024-11-12 23:51:05 +0530

    Initial Commit

---
+++ src/main/java/com/poojithairosha/ecodeals/EcoDealsApplication.java
@@ -0,0 +1,13 @@
+package com.poojithairosha.ecodeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoDealsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoDealsApplication.class, args);
+    }
+
+}
